2b1ab50add5ca927ac4e99d188481f5ef2f6e2e3
Alihaydar Gubatov <gubatovalihaydar@gmail.com>
2021-05-26 02:47:18 +0400

    Added models for rooms, apartments, move-outs

---
+++ moveoutlist/places/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from utils.BaseModels import BaseModel
+
+USER_MODEL = get_user_model()
+
+
+class Address(BaseModel):
+    """A model for storing the address data, address is any place that has rooms for rent"""
+    owner = models.ForeignKey(USER_MODEL, verbose_name=_("Owner"), on_delete=models.SET_NULL, null=True, blank=False)
+    title = models.CharField(verbose_name=_("Title"), max_length=100)
+    description = models.TextField(verbose_name=_("Description"), max_length=500)
+    location = models.CharField(max_length=100)
+
+
+class Room(BaseModel):
+    """A single room of a house(address)"""
+    address = models.ForeignKey("Address", verbose_name=_("address"), on_delete=models.CASCADE)
+    title = models.CharField(verbose_name=_("Title"), max_length=50)
+
+
+class MoveOut(BaseModel):
+    """Table for holding data of move-outs"""
+    move_out_date = models.DateField(verbose_name="Move-out date")
+    room = models.ForeignKey("Room", on_delete=models.SET_NULL, null=True, blank=False)
+    last_occupant = models.ForeignKey(USER_MODEL, on_delete=models.SET_NULL, null=True, blank=False)
