09264d295faba862d4625986b13e6cbe97aca659
Alihaydar Gubatov <gubatovalihaydar@gmail.com>
2021-05-26 16:44:01 +0400

    Added address serializer

--- moveoutlist/places/api/serializers.py
+++ moveoutlist/places/api/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 
-from places.models import MoveOut
+from places.models import MoveOut, Address
+
+
+class AddressGetSerializer(serializers.ModelSerializer):
+    """Serializer for showing address data in Move-out list"""
+
+    class Meta:
+        model = Address
+        fields = ('id', 'title', 'location')
 
 
 class MoveOutGetSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,4 +20,6 @@ class MoveOutGetSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = MoveOut
-        fields = ('move_out_date', 'id', 'address', 'room', 'location', 'last_occupant', 'balance')
+        fields = ('move_out_date', 'id', 'address', 'room', 'location', 'last_occupant', 'uid', 'balance')
+
+    # def get_address(self, obj):
