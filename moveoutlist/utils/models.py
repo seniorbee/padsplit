977a638ac8898feb8d09c03eddf25929205422b6
Alihaydar Gubatov <gubatovalihaydar@gmail.com>
2021-05-29 21:35:51 +0400

    made utils an app, because jwt package was breaking

---
+++ moveoutlist/utils/models.py
@@ -0,0 +1,15 @@
+import uuid
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+# Create your models here.
+class BaseModel(models.Model):
+    """Base model for providing uuid, updated_at, created_at fields"""
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    update_at = models.DateTimeField(auto_now=True, verbose_name=_("Updated date"))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created date"))
+
+    class Meta:
+        abstract = True
